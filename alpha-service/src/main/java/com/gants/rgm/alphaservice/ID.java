96fad914c49087af6173b4b47ed0e7200eaea7b7
bgants <bobgants@gmail.com>
2020-12-18 17:39:45 -0500

    Added config server capabilites:
    - added spring cloud config server
    - added spring cloud client
    - added a couple of services to helm charts

--- alpha-service/src/main/java/com/gants/rgm/alphaservice/ID.java
+++
@@ -1,9 +0,0 @@
-package com.gants.rgm.alphaservice;
-
-public class ID {
-  private static int counter;
-
-  public static synchronized int getId() {
-    return counter++;
-  }
-}
--- alpha-service/src/main/java/com/gants/rgm/alphaservice/Result.java
+++
@@ -1,6 +0,0 @@
-package com.gants.rgm.alphaservice;
-
-public class Result {
-   int left;
-   int right;
-}
