d3a1a13141fcd96d75a399cde92b83d784c44506
bgants <bobgants@gmail.com>
2020-12-21 14:56:35 -0500

    Configured Eureka client and introduced into the mix:
    - Added alpha-service to register with eureka
    - Correctly brought in configs for eureka

--- alpha-service/src/main/java/com/gants/rgm/alphaservice/AlphaServiceApplication.java
+++ alpha-service/src/main/java/com/gants/rgm/alphaservice/AlphaServiceApplication.java
@@ -4,10 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @Slf4j
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableDiscoveryClient
 public class AlphaServiceApplication {
 
 	public static void main(String[] args) {
